b7825a8d8d6cf6aa979ee18f3f6b8c5bea4639e4
Matheus Amon <amonmatheus757@gmail.com>
2024-05-09 15:58:00 -0300

    feat: add background container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css'
+import BackgroundContainer from './components/background-container'
 import Form from './components/form'
 
 function App() {
 
   return (
-    <>
+    <div className='pb-20'>
+      <BackgroundContainer />
       <Form>
         <h1 className='pb-6 text-3xl font-sans font-bold text-primary'>Formulário de Contato</h1>
       </Form>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/background-container.tsx
@@ -0,0 +1,9 @@
+const BackgroundContainer = () => {
+    return ( 
+        <div className="w-full h-[320px]">
+            <img src="/background.png" alt="Plano de fundo de um computador" className="h-full" />
+        </div>
+     );
+}
+ 
+export default BackgroundContainer;
